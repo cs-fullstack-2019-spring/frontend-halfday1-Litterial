bfb9d09ca6a1fe673b32a3501fb8b06ebf87d084
Litterial <yungice12@gmail.com>
2019-02-11 10:20:42 -0600

    working on p and q buttons

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var background=document.getElementById('backbone');
-var pandQ=document.getElementById("keyboard");
-var clicking=document.getElementById("clicks");
+
+
+
+
+
+/*defining ids for background buttons*/
 var changeBlue=document.getElementById('blueB');
 var changeRed=document.getElementById('redB');
 var changeYellow=document.getElementById('yellowB');
-var buttonp1=document.getElementById('inputP1');
-var buttonp2=document.getElementById('inputP2');
-var changeLeft=document.getElementById('counter1');
-var changeRight=document.getElementById('counter2');
-
-
+/*event listeners for background*/
 changeBlue.addEventListener('click',azul);
 changeRed.addEventListener('click',rojo);
 changeYellow.addEventListener('click',amarillo);
 
+/*functions for background*/
 function rojo()
 {
     background.classList.add('red');
@@ -34,11 +34,53 @@ function amarillo()
     background.classList.remove('red');
 }
 
+
+
+
+var pandQ=document.getElementById("keyboard");
+var clicking=document.getElementById("clicks");
+clicking.addEventListener('click',turnOnKeyboard);
+pandQ.addEventListener('click',turnOnMouse);
+pandQ.setAttribute('disabled',false);
+
+function turnOnKeyboard(e)
+{
+    pandQ.removeAttribute('disabled');
+    buttonp1.removeEventListener('click');
+    buttonp2.removeEventListener('click');
+
+    if (e.code=='p')
+    {
+        p1count++;
+        changeLeft.innerText=p1count;
+    }
+
+    else if (e.code=='q')
+    {
+        p2count++;
+        changeRight.innerText=p2count;
+    }
+
+}
+
+
+
+/*Player 1 and Player 2 puttons*/
+var buttonp1=document.getElementById('inputP1');
+var buttonp2=document.getElementById('inputP2');
+/*P tags for counters*/
+var changeLeft=document.getElementById('counter1');
+var changeRight=document.getElementById('counter2');
+/*Event listeners for Player 1 and 2 buttons*/
 buttonp1.addEventListener('click',up1);
 buttonp2.addEventListener('click',up2);
+
+/*Score for players*/
 p1count=0;
 p2count=0;
 
+
+/*increments count for players*/
 function up1()
 {
 
