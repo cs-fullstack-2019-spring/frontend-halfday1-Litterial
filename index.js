a5376538fb70405f9fe3808b5644df0eda51e2ba
Litterial <yungice12@gmail.com>
2019-02-11 11:27:00 -0600

    forgot to do scorekeeping for the key presses. Opps.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var background=document.getElementById('backbone');
-var scoreKeeper=document.getElementById('score');
 
 
 
@@ -50,6 +49,9 @@ var buttonp2=document.getElementById('inputP2');
 /*P tags for counters*/
 var changeLeft=document.getElementById('counter1');
 var changeRight=document.getElementById('counter2');
+
+var scoreKeeper=document.getElementById('score');    /*Scorekeeper for both players*/
+
 /*Event listeners for Player 1 and 2 buttons*/
 buttonp1.addEventListener('click',up1);
 buttonp2.addEventListener('click',up2);
@@ -66,19 +68,21 @@ function up1()
     p1count++;
     changeLeft.innerText=p1count;
 
-    if (p1count>p2count)
+    if (p1count>p2count)    /*when player 1 leads*/
     {
         scoreKeeper.innerText="Player 1"
     }
 
-    else
+    else    /*otherwise*/
     {
         scoreKeeper.innerText="Tie"
     }
 
-    if (p1count==15)
+    if (p1count==15)    /*when player 1 hits 15*/
     {
         alert("Player 1 is the winner.");
+
+        /*reset the input counts and resets scoreboard*/
         p1count=0;
         p2count=0;
         changeLeft.innerText=0;
@@ -91,7 +95,7 @@ function up2()
 {
     p2count++;
     changeRight.innerText=p2count;
-    if (p2count>p1count)
+    if (p2count>p1count)   /*when player 2 leads*/
     {
         scoreKeeper.innerText="Player 1"
     }
@@ -104,6 +108,8 @@ function up2()
     if(p2count==15)
     {
         alert("Player 2 is the winner.");
+
+        /*reset the input counts and resets scoreboard*/
         p1count=0;
         p2count=0;
         changeLeft.innerText=0;
@@ -116,32 +122,31 @@ function up2()
 
 
 addEventListener('load',clickingDefault);   /*set up to have clicking on default*/
-var pandQ=document.getElementById("keyboard");
-var clicking=document.getElementById("clicks");
-clicking.addEventListener('click',turnOffKeyboard);
-pandQ.addEventListener('click',turnOnKeyboard);
-/*
-addEventListener('keydown',readKeyboard);
-*/
+var pandQ=document.getElementById("keyboard");     /*id for the p and q options*/
+var clicking=document.getElementById("clicks");     /*id for the clicking option*/
+clicking.addEventListener('click',turnOffKeyboard);         /*turn off keyboard the p and q options*/
+pandQ.addEventListener('click',turnOnKeyboard);             /*turn on the p and q options*/
 
 function clickingDefault()   /*turns off keyboard on load*/
 {
     removeEventListener('keydown',readKeyboard);
-    clicking.setAttribute('disabled',true);
+    clicking.setAttribute('disabled',true);   /*grays out the clicking button to notify user what you are using*/
 
 }
 
 function turnOnKeyboard()
 {
-    addEventListener('keydown',readKeyboard);
+    addEventListener('keydown',readKeyboard);     /*turns on keyboard commands*/
+               /*turns off the P1 and P2 buttons*/
     buttonp1.removeEventListener('click',up1);
     buttonp2.removeEventListener('click',up2);
-    clicking.removeAttribute('disabled');
-    pandQ.setAttribute('disabled',true);
+    clicking.removeAttribute('disabled');           /*ungrays the clicking button*/
+    pandQ.setAttribute('disabled',true);       /*grays out the  p and q button*/
 }
 
 function turnOffKeyboard()
 {
+    /*turns the P1 and P2 buttons back on*/
     buttonp1.addEventListener('click',up1);
     buttonp2.addEventListener('click',up2);
     removeEventListener('keydown',readKeyboard);
@@ -149,21 +154,18 @@ function turnOffKeyboard()
     clicking.setAttribute('disabled',true);
 }
 
-function readKeyboard(e)
-{
+function readKeyboard(e) {
 
-    if (e.code==='KeyP')
+    if (e.code === 'KeyP')   /*reads player 1's key*/
     {
-        p1count++;
-        changeLeft.innerText=p1count;
-    }
+        up1()  /*calls scorekeeping for player 1*/
 
-    else if (e.code==='KeyQ')
+    } else if (e.code === 'KeyQ')   /*reads player 2's key*/
     {
-        p2count++;
-        changeRight.innerText=p2count;
-    }
+        up2()   /*calls scorekeeping for player 2*/
 
+
+    }
 }
 
 
